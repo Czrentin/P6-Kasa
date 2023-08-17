7e286aa48f722341ed78a3be316bc0edd53d5d22
Corentin Braud <corentin.braud95@gmail.com>
2023-08-17 13:31:11 +0200

    Changement if/else en ternaire

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -2,21 +2,13 @@ import ImageHome from "../assets/background-image-1.png"
 import ImageAbout from "../assets/background-image-2.png"
 
 function BackgroundImage({ page, content }) {
-  let imageSrc = null
-
-  // Utilisez une condition pour déterminer quelle image afficher en fonction de la page
-  if (page === "home") {
-    imageSrc = ImageHome
-  } else if (page === "about") {
-    imageSrc = ImageAbout
-  }
+  const imageSrc = page === "home" ? ImageHome : ImageAbout
 
   return (
     <div className="bg-container">
       <div className="bg-content">
         <p>{content}</p>
       </div>
-
       <img src={imageSrc} alt={"Paysage"} />
     </div>
   )
